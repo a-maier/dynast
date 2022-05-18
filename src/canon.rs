36e804bc7b6c128f328dbcef2edc08ae96cfe54a
Andreas Maier <andreas.martin.maier@desy.de>
2022-05-18 15:03:55 +0200

    move around graph utility functions

--- src/canon.rs
+++ src/canon.rs
@@ -1,17 +1,12 @@
-use std::ops::AddAssign;
-use std::convert::identity;
-use std::cmp::Ordering;
-
 use derivative::Derivative;
 use log::trace;
 use nauty_pet::prelude::*;
 use petgraph::{
-    graph::{IndexType, UnGraph},
-    visit::EdgeRef,
-    EdgeType, Graph, Undirected,
+    graph::UnGraph,
+    Undirected,
 };
 
-use crate::graph_util::Format;
+use crate::graph_util::{Format, contract_graph_edge, from_nodes_edges, transform_nodes, into_nodes_edges};
 use crate::momentum::Momentum;
 use crate::yaml_dias::EdgeWeight;
 
@@ -83,134 +78,10 @@ fn relabel_nodes(
     from_nodes_edges(nodes, edges)
 }
 
-fn into_nodes_edges<N, E, Ty, Ix>(
-    g: Graph<N, E, Ty, Ix>,
-) -> (Vec<N>, Vec<(usize, usize, E)>)
-where
-    Ty: EdgeType,
-    Ix: IndexType,
-{
-    into_transformed_nodes_edges(g, identity, identity)
-}
-
-fn from_nodes_edges<N, E, Ty, Ix>(
-    nodes: Vec<N>,
-    edges: Vec<(usize, usize, E)>,
-) -> Graph<N, E, Ty, Ix>
-where
-    Ty: EdgeType,
-    Ix: IndexType,
-{
-    let mut res = Graph::with_capacity(nodes.len(), edges.len());
-    for node in nodes {
-        res.add_node(node);
-    }
-    for edge in edges {
-        use petgraph::visit::NodeIndexable;
-        let from = res.from_index(edge.0);
-        let to = res.from_index(edge.1);
-        res.add_edge(from, to, edge.2);
-    }
-    res
-}
-
-fn into_transformed_nodes_edges<N, E, Ty, Ix, F, G, NN, EE>(
-    g: Graph<N, E, Ty, Ix>,
-    mut node_transform: F,
-    mut edge_transform: G,
-) -> (Vec<NN>, Vec<(usize, usize, EE)>)
-where
-    Ty: EdgeType,
-    Ix: IndexType,
-    F: FnMut(N) -> NN,
-    G: FnMut(E) -> EE,
-{
-    use petgraph::visit::NodeIndexable;
-    let edges = Vec::from_iter(
-        g.edge_references()
-            .map(|e| (g.to_index(e.source()), g.to_index(e.target()))),
-    );
-    let (nodes, e) = g.into_nodes_edges();
-    let nodes = nodes
-        .into_iter()
-        .map(|n| node_transform(n.weight))
-        .collect();
-    let edges = edges
-        .into_iter()
-        .zip(e.into_iter().map(|e| e.weight))
-        .map(|((from, to), wt)| (from, to, edge_transform(wt)))
-        .collect();
-    (nodes, edges)
-}
-
-fn transform_nodes_edges<N, NN, E, EE, Ty, Ix, F, G>(
-    g: Graph<N, E, Ty, Ix>,
-    node_transform: F,
-    edge_transform: G,
-) -> Graph<NN, EE, Ty, Ix>
-where
-    Ty: EdgeType,
-    Ix: IndexType,
-    F: FnMut(N) -> NN,
-    G: FnMut(E) -> EE,
-{
-    let (nodes, edges) =
-        into_transformed_nodes_edges(g, node_transform, edge_transform);
-    from_nodes_edges(nodes, edges)
-}
-
-fn transform_nodes<N, NN, E, Ty, Ix, F>(
-    g: Graph<N, E, Ty, Ix>,
-    node_transform: F,
-) -> Graph<NN, E, Ty, Ix>
-where
-    Ty: EdgeType,
-    Ix: IndexType,
-    F: FnMut(N) -> NN,
-{
-    transform_nodes_edges(g, node_transform, identity)
-}
-
-fn contract_graph_edge<N, E, Ty, Ix>(
-    g: Graph<N, E, Ty, Ix>,
-    idx: usize
-) -> Graph<N, E, Ty, Ix>
-where
-    N: AddAssign,
-    Ty: EdgeType,
-    Ix: IndexType,
-{
-    let (mut nodes, mut edges) = into_nodes_edges(g);
-    let contracted = edges.remove(idx);
-    let (merged, removed) = minmax(contracted.0, contracted.1);
-    if merged != removed {
-        let removed_node = nodes.remove(removed);
-        nodes[merged] += removed_node;
-        for edge in &mut edges {
-            for vx in [&mut edge.0, &mut edge.1] {
-                match removed.cmp(vx) {
-                    Ordering::Less => *vx -= 1,
-                    Ordering::Equal => *vx = merged,
-                    Ordering::Greater => { }
-                }
-            }
-        }
-    }
-    from_nodes_edges(nodes, edges)
-}
-
 pub(crate) fn contract_edge(
     g: CanonGraph<Momentum, EdgeWeight, Undirected>,
     idx: usize
 ) -> CanonGraph<Momentum, EdgeWeight, Undirected>
 {
     into_canon(contract_graph_edge(g.into(), idx))
-}
-
-fn minmax<T: Ord>(s: T, t: T) -> (T, T) {
-    if s < t {
-        (s, t)
-    } else {
-        (t, s)
-    }
 }
--- src/graph_util.rs
+++ src/graph_util.rs
@@ -1,8 +1,13 @@
+use std::convert::identity;
+use std::cmp::Ordering;
 use std::fmt::{self, Display};
+use std::ops::AddAssign;
 
 use num_traits::Zero;
 use petgraph::{
-    graph::UnGraph,
+    EdgeType,
+    graph::{IndexType, UnGraph},
+    Graph,
     visit::{EdgeRef, NodeIndexable},
 };
 
@@ -44,5 +49,127 @@ impl<'a> Display for FormatUnGraph<'a> {
             writeln!(f, "      [({from}, {to}), {}, {}],", e.weight().p, e.weight().m)?
         }
         writeln!(f, "   ],\n}}")
+    }
+}
+
+pub(crate) fn into_nodes_edges<N, E, Ty, Ix>(
+    g: Graph<N, E, Ty, Ix>,
+) -> (Vec<N>, Vec<(usize, usize, E)>)
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+{
+    into_transformed_nodes_edges(g, identity, identity)
+}
+
+pub(crate) fn from_nodes_edges<N, E, Ty, Ix>(
+    nodes: Vec<N>,
+    edges: Vec<(usize, usize, E)>,
+) -> Graph<N, E, Ty, Ix>
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+{
+    let mut res = Graph::with_capacity(nodes.len(), edges.len());
+    for node in nodes {
+        res.add_node(node);
+    }
+    for edge in edges {
+        let from = res.from_index(edge.0);
+        let to = res.from_index(edge.1);
+        res.add_edge(from, to, edge.2);
+    }
+    res
+}
+
+pub(crate) fn into_transformed_nodes_edges<N, E, Ty, Ix, F, G, NN, EE>(
+    g: Graph<N, E, Ty, Ix>,
+    mut node_transform: F,
+    mut edge_transform: G,
+) -> (Vec<NN>, Vec<(usize, usize, EE)>)
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+    F: FnMut(N) -> NN,
+    G: FnMut(E) -> EE,
+{
+    let edges = Vec::from_iter(
+        g.edge_references()
+            .map(|e| (g.to_index(e.source()), g.to_index(e.target()))),
+    );
+    let (nodes, e) = g.into_nodes_edges();
+    let nodes = nodes
+        .into_iter()
+        .map(|n| node_transform(n.weight))
+        .collect();
+    let edges = edges
+        .into_iter()
+        .zip(e.into_iter().map(|e| e.weight))
+        .map(|((from, to), wt)| (from, to, edge_transform(wt)))
+        .collect();
+    (nodes, edges)
+}
+
+pub(crate) fn transform_nodes_edges<N, NN, E, EE, Ty, Ix, F, G>(
+    g: Graph<N, E, Ty, Ix>,
+    node_transform: F,
+    edge_transform: G,
+) -> Graph<NN, EE, Ty, Ix>
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+    F: FnMut(N) -> NN,
+    G: FnMut(E) -> EE,
+{
+    let (nodes, edges) =
+        into_transformed_nodes_edges(g, node_transform, edge_transform);
+    from_nodes_edges(nodes, edges)
+}
+
+pub(crate) fn transform_nodes<N, NN, E, Ty, Ix, F>(
+    g: Graph<N, E, Ty, Ix>,
+    node_transform: F,
+) -> Graph<NN, E, Ty, Ix>
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+    F: FnMut(N) -> NN,
+{
+    transform_nodes_edges(g, node_transform, identity)
+}
+
+pub(crate) fn contract_graph_edge<N, E, Ty, Ix>(
+    g: Graph<N, E, Ty, Ix>,
+    idx: usize
+) -> Graph<N, E, Ty, Ix>
+where
+    N: AddAssign,
+    Ty: EdgeType,
+    Ix: IndexType,
+{
+    let (mut nodes, mut edges) = into_nodes_edges(g);
+    let contracted = edges.remove(idx);
+    let (merged, removed) = minmax(contracted.0, contracted.1);
+    if merged != removed {
+        let removed_node = nodes.remove(removed);
+        nodes[merged] += removed_node;
+        for edge in &mut edges {
+            for vx in [&mut edge.0, &mut edge.1] {
+                match removed.cmp(vx) {
+                    Ordering::Less => *vx -= 1,
+                    Ordering::Equal => *vx = merged,
+                    Ordering::Greater => { }
+                }
+            }
+        }
+    }
+    from_nodes_edges(nodes, edges)
+}
+
+fn minmax<T: Ord>(s: T, t: T) -> (T, T) {
+    if s < t {
+        (s, t)
+    } else {
+        (t, s)
     }
 }
