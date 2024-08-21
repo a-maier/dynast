93890f7d6f3bc0d6d0605532224557777ac65f57
Andreas Maier <andreas.martin.maier@desy.de>
2024-08-21 09:55:23 +0200

    fix regression after vergen update

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
 use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() {
-    let gitcl = GitclBuilder::default().sha(true).build().unwrap();
+    let gitcl = GitclBuilder::default()
+        .branch(true)
+        .sha(true)
+        .build()
+        .unwrap();
     // If this is not run inside a git repository we get an error.
     // This happens when installing the crate via cargo.
     // As a quick fix, we just ignore it.
-    let _ = Emitter::default()
+     let _ = Emitter::default()
         .add_instructions(&gitcl).unwrap()
         .emit();
 }
--- src/version.rs
+++ src/version.rs
@@ -3,7 +3,7 @@ use std::sync::LazyLock;
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub const GIT_REV: Option<&str> = option_env!("VERGEN_GIT_SHA_SHORT");
+pub const GIT_REV: Option<&str> = option_env!("VERGEN_GIT_SHA");
 pub const GIT_BRANCH: Option<&str> = option_env!("VERGEN_GIT_BRANCH");
 
 pub static VERSION_MAJOR: LazyLock<u32> = LazyLock::new(||
