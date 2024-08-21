6ff8a3b2aeb1829655eb86aecc8e232e84eadbf0
Andreas Maier <andreas.martin.maier@desy.de>
2024-08-21 09:34:48 +0200

    update dependencies

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
-use vergen::EmitBuilder;
+use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() {
+    let gitcl = GitclBuilder::default().sha(true).build().unwrap();
     // If this is not run inside a git repository we get an error.
     // This happens when installing the crate via cargo.
     // As a quick fix, we just ignore it.
-    let _ = EmitBuilder::builder().git_sha(true).emit();
+    let _ = Emitter::default()
+        .add_instructions(&gitcl).unwrap()
+        .emit();
 }
